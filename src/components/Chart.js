851c152217ffa947823b01bd938a80bbc58f90a8
fj <castelloiv@gmail.com>
2020-11-09 16:29:14 -0500

    Chart.js starting work

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,7 +1,52 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
+import ChartJS from "chart.js"
+import Styles from "../styles/chart.module.scss"
 
-function Chart() {
-  return <div></div>
+const Chart = () => {
+  const chartRef = useRef()
+
+  useEffect(() => {
+    if (chartRef && chartRef.current) {
+      const chartInstance = new ChartJS(chartRef.current, {
+        type: "line",
+        data: {
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [
+                { x: 1, y: 15 },
+                { x: 2, y: 12 },
+                { x: 3, y: 9 },
+                { x: 4, y: 22 },
+              ],
+              backgroundColor: "rgb(131, 245, 169)",
+
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      })
+    }
+  }, [])
+  return (
+    <div className={Styles.wrapper}>
+      <div></div>
+      <div>
+        <canvas ref={chartRef} id="myChart" width={250} height={250} />
+      </div>
+    </div>
+  )
 }
 
 export default Chart
--- src/pages/CoinInfo.js
+++ src/pages/CoinInfo.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Chart from "../components/Chart"
 import Details from "../components/Details"
@@ -7,33 +7,64 @@ import gecko from "../apis/gecko"
 
 function CoinInfo() {
   const { name } = useParams()
+  const [coinData, setCoinData] = useState({})
+  const [time, setTime] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
 
+  const howMany = time | "3"
+  console.log(howMany)
   useEffect(() => {
+    setIsLoading(true)
     const fetchData = async () => {
+      let days
+      let main
       try {
-        const response = await gecko.get(`/coins/${name}/market_chart`, {
-          params: {
-            vs_currency: "usd",
-            days: "1",
-          },
+        ;[days, main] = await Promise.all([
+          gecko.get(`/coins/${name}/market_chart`, {
+            params: {
+              vs_currency: "usd",
+              days: { howMany },
+            },
+          }),
+          gecko.get(`/coins/markets/`, {
+            params: {
+              vs_currency: "usd",
+              ids: name,
+            },
+          }),
+        ])
+        console.log(days, main)
+        setCoinData({
+          days: days.data.prices,
+          main: main.data[0],
         })
-        console.log(response.data)
+        console.log(coinData)
+        setIsLoading(false)
       } catch (error) {
         console.log(error)
+        setIsLoading(false)
       }
     }
     fetchData()
   }, [])
 
   const renderData = () => {
-    return (
-      <div>
-        <div className={Styles.container}>
-          <Chart />
-          <Details />
+    if (isLoading) {
+      return (
+        <div className={Styles.main}>
+          <div className={Styles.container}>Spinner needed</div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div>
+          <div className={Styles.container}>
+            <Chart />
+            <Details />
+          </div>
+        </div>
+      )
+    }
   }
   return renderData()
 }
